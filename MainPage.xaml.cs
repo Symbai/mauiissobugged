a36feb1c61cbbf82a9a1b826ba14191db6d1f2ec
Symbai <14368203+Symbai@users.noreply.github.com>
2024-01-02 09:36:13 +0100

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SwipeGestureRecognizer_Swiped(object sender, SwipedEventArgs e)
+        {
+            var s = string.Empty;
+        }
+    }
+
+    public class Bla
+    {
+        public static Bla Instance { get; } = new Bla();
+
+        public Bla()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Items.Add($"Item {i}");
+            }
+        }
+
+        public ObservableCollection<string> Items { get; } = new ObservableCollection<string>();
+
+    }
+
+}
